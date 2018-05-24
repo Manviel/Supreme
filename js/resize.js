334e64c2316f31655420153304e1c9706e4b3e92
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-05-24 12:30:04 +0300

    Start for tablet

--- js/resize.js
+++ js/resize.js
@@ -1,19 +1,38 @@
 let media_size = screen.width;
 
 window.onload = function () {
-  switchClass();
+  switchTab();
+  switchMob();
 
   window.resize = function () {
     media_size = screen.width;
-    switchClass();
+    switchTab();
+    switchMob();
   };
 };
 
-function switchClass() {
-  const before = document.getElementById("b1");
-  const after = document.getElementById("b2");
+function switchMob() {
+  const before = document.getElementById("before");
+  const after = document.getElementById("after");
 
-  if (media_size < 768) {
+  if (media_size <= 375) {
     before.parentNode.insertBefore(after, before);
+  }
+}
+
+function switchTab() {
+  let sale = document.getElementById("sale");
+  let h3 = sale.nextElementSibling.children[0];
+  let i = sale.nextElementSibling.children[1];
+
+  if (media_size == 768) {
+    sale.src = "./img/sale.png";
+    h3.innerText = "Sale";
+    h3.classList.add("red");
+    i.innerText = "-70%";
+  } else {
+    sale.src = "./img/right.png";
+    h3.innerText = "Caps & Hats";
+    i.innerText = "Sale -50%";
   }
 }
